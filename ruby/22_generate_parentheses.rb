da5984417270cef4dde8b7625ce5a9226a30dcac
longjianjiang <616393956@qq.com>
2018-08-10 00:00:03 +0800

    add generate parentheses use ruby

---
+++ ruby/22_generate_parentheses.rb
@@ -0,0 +1,37 @@
+# @param {Integer} n
+# @return {String[]}
+
+
+# 给定n求n对括号的所有组合情况
+# 想到使用递归，递归和普通函数没什么区别，只是注意回溯，同时注意n 和 n-1的关系
+# _generate_parenthesis_(left, right, str, res) 方法是创建有效字符串，并存入数组
+# left，right 分别代表左右括号的个数，left，right肯定等于n，此时字符串中还没有左括号和右括号
+# 当left > right , 此时表示左括号的个数小于右括号个数，这时右括号在前面，此时括号肯定是不合法的，所以可以直接返回
+# 当left，right等于0， 此时可以将字符串插入数组
+# 当left > 0, right > 0时，依次拼接左括号和右括号，同时对left，right进行减减
+# 通过一系列递归，最后即可完整的得到数组
+
+
+def generate_parenthesis(n)
+  [].tap do |result|
+  	_generate_parenthesis_(n, n, '', result)
+  end 
+end
+
+def _generate_parenthesis_(left, right, str, res)
+	
+	puts "left = #{left}, right=#{right}, str=#{str}, res = #{res.inspect}"
+
+	return if left > right 
+	if left == 0 && right == 0
+		res << str
+	else 
+		_generate_parenthesis_(left-1, right, str + '(', res) if left > 0
+		
+		_generate_parenthesis_(left, right-1, str + ')', res) if right > 0
+	end
+end
+
+
+puts generate_parenthesis(2).inspect
+
