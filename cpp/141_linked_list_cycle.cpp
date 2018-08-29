44b1a260e8165e84dc25def61514371310e4d481
longjianjiang <2511067577@qq.com>
2018-08-29 20:09:15 +0800

    add linked list cycle use cpp

---
+++ cpp/141_linked_list_cycle.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+   public:
+   bool hasCycle(ListNode *head) {
+        if (NULL == head) return false;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        bool isCycle = false;
+       
+        while(slow && fast) {
+            slow = slow->next;
+            fast = fast->next;
+   
+            if (NULL == fast) {
+                return false;
+            } else {
+                fast = fast->next;
+            }
+
+            if (fast == slow) {
+                isCycle = true;
+                break;
+            }
+        }
+
+        return isCycle; 
+   }
+};
+
+
+int main() {
+   return 0;
+}
+
+
