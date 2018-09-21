e239f30074b6cdd18f46cf67d3c368cfc9146614
longjianjiang <616393956@qq.com>
2018-09-21 23:16:03 +0800

    complete partition list use ruby

--- ruby/86_partition_list.rb
+++ ruby/86_partition_list.rb
@@ -18,6 +18,21 @@ end
 # 划分列表，将小于x的节点放在前面，大于等于x的节点保持原始顺序
 # 将原链表中小于x的存到一个新的链表，同时原链表中移除该节点，最后将新链表拼接在原链表之前即可
 
+
+def append_list(head, tail, node, vars)
+	if head.nil?
+		eval "head = node", vars
+		eval "tail = node", vars
+		# head, tail = node, node
+	else
+		eval "tail.next = node", vars
+		eval "tail = node", vars
+		# tail.next = node
+		# tail = node
+	end
+end
+
+
 def partition(head, x)
     return head if head.nil? || head.next.nil?
 
@@ -27,11 +42,8 @@ def partition(head, x)
     	node = head
     	head = head.next
     	node.next = nil
-    	append_list(left, right, node)
+    	append_list(left, right, node, binding)
     end
-
-    puts show_list_node_val(left)
-    puts show_list_node_val(right)
 
     if head
     	p = head
@@ -40,30 +52,45 @@ def partition(head, x)
     			node = p.next
     			p.next = node.next
     			node.next = nil
-    			append_list(left, right, node)
+    			append_list(left, right, node, binding)
     		else
     			p = p.next
     		end
     	end
     end
 
-    puts show_list_node_val(head)
-    puts show_list_node_val(left)
-
-    append_list(left, right, node)
+    append_list(left, right, node, binding)
 
     left
 end
 
 
-def append_list(head, tail, node)
-	if head.nil?
-		head, tail = node, node
-	else
-		tail.next = node
-		tail = node
+# 思路一样，之前那种方法初始化为nil，所以导致了判断是否为nil
+# 所以此时可以换种思路，初始化Nodeval为-1，返回的时候，返回next即可，这样即可省去之前的判断
+
+def partition_version_2(head, x)
+	return head if head.nil? || head.next.nil?
+
+	left, right = ListNode.new(-1), ListNode.new(-1)
+	left.next = head
+
+	p, q = left, right
+	while p.next
+		if p.next.val < x
+			q.next = p.next
+			q = q.next
+			p.next = p.next.next
+			q.next = nil
+		else
+			p = p.next
+		end
 	end
+
+	q.next = left.next
+
+	right.next
 end
+
 
 
 def show_list_node_val(node)
@@ -78,6 +105,16 @@ def show_list_node_val(node)
 end
 
 l1 = ListNode.new(1, ListNode.new(4, ListNode.new(3, ListNode.new(2, ListNode.new(5, ListNode.new(2, nil))))))
-l2 = partition(l1, 3)
-
+l2 = partition_version_2(l1, 3)
 puts show_list_node_val(l2)
+
+
+# def swap(a, b, vars)
+# 	tmp = a
+# 	eval "a = b", vars
+# 	eval "b = #{tmp}", vars
+# end
+
+# a, b = 5, 7
+# swap(a, b, binding)
+# puts "a = #{a}, b = #{b}"
