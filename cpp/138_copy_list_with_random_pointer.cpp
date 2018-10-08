0bf3ec9255971e6ed522cfaf4e6224de42a8994f
longjianjiang <616393956@qq.com>
2018-10-08 08:35:48 +0800

    138 add new solution not use map

--- cpp/138_copy_list_with_random_pointer.cpp
+++ cpp/138_copy_list_with_random_pointer.cpp
@@ -43,4 +43,40 @@ public:
 
     	return res;
     }
+
+    // 前面一种方法使用了map，下面这种方法则不需要额外的空间，参考自（https://fisherlei.blogspot.com/2013/11/leetcode-copy-list-with-random-pointer.html）
+    // 分三步：
+    // 1. 首先在原链表中每个节点后复制一个节点
+    // 2. 将复制的节点根据原链表中的random指定到对应的复制节点
+    // 3. 最后将链表断开后，返回复制的节点链表即可
+
+    RandomListNode *copyRandomListVersion2(RandomListNode *head) {
+    	if (!head) return head;
+
+    	RandomListNode *cur = head;
+    	while(cur) {
+    		RandomListNode *t = new RandomListNode(cur->label);
+    		t->next = cur->next;
+    		cur->next = t;
+    		cur = t->next;
+    	}
+
+    	cur = head;
+    	while(cur) {
+    		RandomListNode *random = cur->random;
+    		if (random) cur->next->random = random->next;
+    		cur = cur->next->next;
+    	}
+
+    	cur = head;
+    	RandomListNode *res = cur->next;
+    	while(cur) {
+    		RandomListNode *t = cur->next;
+    		cur->next = t->next;
+    		if (t->next) t->next = t->next->next;
+    		cur = cur->next;
+    	}
+
+    	return res;
+    }
 };
