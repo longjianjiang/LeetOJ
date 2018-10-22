c28a2a8bf721e40b811400859187710458fd0816
longjianjiang <616393956@qq.com>
2018-10-22 23:00:14 +0800

    update find minimum in rotated sorted array use ruby

--- ruby/153_find_minimum_in_rotated_sorted_array.rb
+++ ruby/153_find_minimum_in_rotated_sorted_array.rb
@@ -14,3 +14,20 @@ def find_min(nums)
     head = find_nums_head(nums)
     nums[head]
 end
+
+# 我们可以利用上述思路，从left，right两个方向判断，加快速度
+
+def find_min_version2(nums)
+	return -1 if nums.empty?
+	return nums[0] if nums.size == 1 || nums[0] < nums[nums.size-1]
+
+	left, right = 0, nums.size-1
+	while left <= right
+		return nums[left+1] if nums[left+1] < nums[left]
+		return nums[right] if nums[right-1] > nums[right]
+		left += 1
+		right -= 1
+	end
+
+	-1
+end
