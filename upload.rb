2ba450697a038d4051532da0d38c57ae0c7ee9d7
longjianjiang <616393956@qq.com>
2018-10-27 15:07:10 +0800

    Update(auto commit)

--- upload.rb
+++ upload.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #puts "enter commit message"
 #commit_msg = gets.chomp
-commit_msg = "auto commit"
+commit_msg = "Update(auto commit)"
 commit_command = "git commit -m '#{commit_msg}' "
 
 `git add .`
