d4767eae0a35f9658a48ed969c5ffec28ec803ff
longjianjiang <616393956@qq.com>
2019-02-01 00:31:17 +0800

    Update(auto commit)

---
+++ addition/offer_4.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 给定二叉树的前序遍历和中序遍历，让我们构建出二叉树返回
+
+class Solution {
+public:
+    TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
+
+    }
+};
--- ruby/69_sqrt(x).rb
+++ ruby/69_sqrt(x).rb
@@ -2,6 +2,21 @@
 # @return {Integer}
 
 # 开平方，只保留整数
+# 当x大于1时，二分[1,x], 判断 x / mid 与 mid 大小关系，最后返回 right-1
 
 def my_sqrt(x)
+	return x if x <= 1
+	left, right = 1, x 
+	while left < right
+		mid = left + (right - left) / 2
+		if x / mid > mid
+			left = mid + 1
+		elsif x / mid == mid
+			return mid
+		else
+			right = mid
+		end
+	end
+
+	right-1
 end
