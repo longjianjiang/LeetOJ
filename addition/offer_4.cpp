88b310e48094f4c889c4b0bb891d5b8610e9d647
longjianjiang <616393956@qq.com>
2019-02-14 17:39:05 +0800

    Update(auto commit)

--- addition/offer_4.cpp
+++ addition/offer_4.cpp
@@ -14,10 +14,36 @@
 using namespace std;
 
 // 给定二叉树的前序遍历和中序遍历，让我们构建出二叉树返回
+// 首先根据先序遍历确定根节点，然后构建左右子数的先序数组和中序数组，递归的用其构建左右子树
 
 class Solution {
 public:
     TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
+        if (pre.size() == 0) return NULL;
+        int treeSize = pre.size();
+        vector<int> leftPreorder, leftInorder, rightPreorder, rightInorder;
 
+        TreeNode *head = new TreeNode(pre[0]);
+        int rootNodeIdxAtInorder = 0;
+        for (int i = 0; i < treeSize; i++) {
+             if (vin[i] == pre[0]) {
+                rootNodeIdxAtInorder = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < rootNodeIdxAtInorder; ++i) {
+            leftInorder.push_back(vin[i]);
+            leftPreorder.push_back(pre[i+1]);
+        }
+        for (int i = rootNodeIdxAtInorder+1; i < treeSize; ++i) {
+            rightInorder.push_back(vin[i]);
+            rightPreorder.push_back(pre[i]);
+        }
+
+        head->left = reConstructBinaryTree(leftPreorder, leftInorder);
+        head->right = reConstructBinaryTree(rightPreorder, rightInorder);
+
+        return head;
     }
 };
