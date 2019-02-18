02a921e3ace873036149671b8968051e918d6c79
longjianjiang <616393956@qq.com>
2019-02-18 20:27:24 +0800

    Update(auto commit)

---
+++ ruby/658_find_k_closest_elements.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} arr
+# @param {Integer} k
+# @param {Integer} x
+# @return {Integer[]}
+
+# 给定有序数组，要求返回k个最接近x的元素，以升序的形式返回，如果两个元素距离x相等，返回较小的那个
+
+def find_closest_elements(arr, k, x)
+    
+end
