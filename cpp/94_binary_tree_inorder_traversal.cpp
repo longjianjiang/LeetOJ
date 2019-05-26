a5c31c0c9632a1e54ca2b0c97e2d7d5ab1442ea5
longjianjiang <616393956@qq.com>
2019-05-26 20:48:12 +0800

    Update(auto commit)

--- cpp/94_binary_tree_inorder_traversal.cpp
+++ cpp/94_binary_tree_inorder_traversal.cpp
@@ -8,8 +8,27 @@
  * };
  */
 // 迭代的方式中序遍历二叉树
+// 利用递归的思路即可，使用栈来保存节点即可
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+		if (!root) { return {}; }
+
+		stack<TreeNode *> s;
+		vector<int> res;
+		auto pointer = root;
+		while (pointer || !s.empty()) {
+			while (pointer) {
+				s.push(pointer);
+				pointer = pointer->left;
+			}
+
+			auto node = s.top();
+			s.pop();
+			res.push_back(node->val);
+			pointer = node->right;
+		}
+
+		return res;
     }
 };
