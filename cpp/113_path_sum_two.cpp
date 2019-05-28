305f1e9e5cf790f76ec814e1eaca31af424d8869
longjianjiang <616393956@qq.com>
2019-05-28 20:34:58 +0800

    Update(auto commit)

---
+++ cpp/113_path_sum_two.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// 和112类似，只是这题要求我们返回构成等于sum的路径数组
+// 使用递归，增加一个数组路径即可
+class Solution {
+public:
+    void dfs(TreeNode* root, int target, int sum, vector<int> chosen, vector<vector<int>>& res) {
+        if (!root) { return; }
+        
+        sum += root->val;
+        chosen.push_back(root->val);
+        if (sum == target && !root->left && !root->right) {
+            res.push_back(chosen);
+            return;
+        }
+        
+        dfs(root->left, target, sum, chosen, res);
+        dfs(root->right, target, sum, chosen, res);
+	}
+    
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<int> chosen;
+        vector<vector<int>> res;
+        dfs(root, sum, 0, chosen, res);
+        
+        return res;
+    }
+};
