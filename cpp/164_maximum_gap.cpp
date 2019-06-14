96a4a6ce7fb4f2e1d939bc9751702c344e79a5f8
longjianjiang <616393956@qq.com>
2019-06-14 11:09:11 +0800

    Update(auto commit)

--- cpp/164_maximum_gap.cpp
+++ cpp/164_maximum_gap.cpp
@@ -3,7 +3,7 @@
 
 // 给定未排序的数组，要求返回排序后两两组合差值最大的。
 // [3, 6, 9, 1] => 3
-
+// 分桶，最大桶和最小桶，最大间隔为相邻的最小桶-最大桶的差值
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
@@ -30,7 +30,19 @@ public:
 			bucketMax[idx] = max(bucketMax[idx], num);
 		}
 
-		
+		// 3-1 / 3 = 0 
+		// bMin[1, 6, 9]
+		// bMax[3, 6 ,9]
+		// bMin[i] - bMax[i-1]
 
+		int res = 0;
+        int lastIdx = 0;
+		for (int i = 1; i < bucketCount; ++i) {
+            if (bucketMin[i] == INT_MAX) { continue; }
+			int diff = bucketMin[i] - bucketMax[lastIdx];
+			res = diff > res ? diff : res;
+            lastIdx = i;
+		}
+		return res;
     }
 };
