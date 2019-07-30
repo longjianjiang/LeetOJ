25613ded63e69022c9b0d7af8985a9a4baab6916
longjianjiang <616393956@qq.com>
2019-07-30 10:33:01 +0800

    Update(auto commit)

--- cpp/101_symmetric_tree.cpp
+++ cpp/101_symmetric_tree.cpp
@@ -12,9 +12,31 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-    	if (!root) { return false; }
+    	if (!root) { return true; }
     	return isSymmetric(root->left, root->right);
     }
+
+	bool isSymmetricIteration(TreeNode* root) {
+		if (!root) { return true; }
+
+		queue<TreeNode *> q1, q2;
+		q1.push(root->left);
+		q2.push(root->right);
+
+		while (!q1.empty() && !q2.empty()) {
+			TreeNode *node1 = q1.front(); q1.pop();
+			TreeNode *node2 = q2.front(); q2.pop();
+
+			if (node1 == nullptr && node2 == nullptr) { continue; }
+			if (node1 == nullptr || node2 == nullptr) { return false; }
+			if (node1->val != node2->val) { return false; }
+
+			q1.push(node1->left); q2.push(node2->right);
+			q1.push(node1->right); q2.push(node2->left);
+		}
+
+		return true;
+	}
 
     bool isSymmetric(TreeNode *left, TreeNode *right) {
     	if (!left && !right) { return true; }
