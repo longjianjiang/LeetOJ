6a9598e89c24ade44815048de1a6aeb6c3c84c21
longjianjiang <616393956@qq.com>
2020-04-02 21:56:23 +0800

    154; bs;

--- cpp/154_find_minimum_in_rotated_sorted_array_two.cpp
+++ cpp/154_find_minimum_in_rotated_sorted_array_two.cpp
@@ -13,6 +13,7 @@ using namespace std;
 // 1 7 7 7 7
 // 7 1 7 7 7
 // 1> 从左右开始遍历，找到逆序的返回，不过首先需要过滤掉重复元素；
+// 2> 二分，因为存在重复，需要进行递归；
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -35,4 +36,32 @@ public:
 
 		return -1;
     }
+
+	int helper(vector<int>& nums, int left, int right) {
+		if (left == right) { return nums[left]; }
+		int mid = left + (right - left) / 2;
+		int l = 0, r = 0;
+
+		if (nums[left] < nums[mid]) {
+			l = nums[left];
+		} else {
+			l = helper(nums, left, mid);
+		}
+		if (nums[mid+1] < nums[right]) {
+			r = nums[mid+1];
+		} else {
+			r = helper(nums, mid+1, right);
+		}
+
+		return min(l, r);
+	}
+
+	int findMin_bs(vector<int>& nums) {
+		if (nums.empty()) { return -1; }
+		// sorted array
+		if (nums.size() == 1 || (nums[0] < nums[nums.size()-1])) { return nums[0]; }
+
+		int left = 0, right = nums.size()-1;
+		return helper(nums, left, right);
+	}
 };
