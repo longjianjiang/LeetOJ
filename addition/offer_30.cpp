5bd6a2d575417d15612051b4c51a5c2972782f0a
longjianjiang <616393956@qq.com>
2020-05-06 12:41:41 +0800

    dp;

--- addition/offer_30.cpp
+++ addition/offer_30.cpp
@@ -9,4 +9,23 @@
 #include <unordered_map>
 using namespace std;
 
-https://www.nowcoder.com/practice/459bd355da1549fa8a49e350bf3df484?tpId=13&tqId=11183&rp=2&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+// 给一个数组，返回它的最大连续子序列的和;
+// [6,-3,-2,7,-15,1,2,2]
+
+// 1> 暴力，两层循环；
+// 2> dp, dp[0] = nums[0]; dp[i] = max(nums[i], nums[i]+dp[i-1]);
+class Solution {
+public:
+    int FindGreatestSumOfSubArray(vector<int> array) {
+		if (array.empty()) { return 0; }
+		if (array.size() == 1) { return array[0]; }
+
+		int dpSum = array[0], maxSum = array[0];
+		for (int i = 1; i < array.size(); ++i) {
+			dpSum = max(array[i], array[i]+dpSum);
+			maxSum = max(dpSum, maxSum);
+		}
+
+		return maxSum;
+    }
+};
