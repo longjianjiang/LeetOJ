e5f83b0da19c0a8ec2899d227694d36b8063f5c8
longjianjiang <616393956@qq.com>
2020-05-16 00:34:52 +0800

    two pointer;

--- addition/offer_41.cpp
+++ addition/offer_41.cpp
@@ -12,53 +12,55 @@ using namespace std;
 // 输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
 
 // 1> 穷举；
-// 10
-// 1-2-3-4
-// 1-3-4-5
-// 1-4-5
-// 1-5-6
-// 1-6-7
-// 1-7-8
-// 1-8-9
-// 1-9
-// 2-3-4-5
-// 2-4-5
-// 2-5-6
-// 2-6
-// 2-7
-// 2-8
-// 3-4-5
-// 3-5
-// 3-6
-// 3-7
-// 4-5
-// 4-6
-// 5-6
-// 6
+// 2> 双指针遍历；
 class Solution {
 public:
     vector<vector<int> > FindContinuousSequence(int sum) {
 		if (sum <= 1) { return {}; }
 
 		vector<vector<int>> res;
-		for (int i = 1; i < sum/2; ++i) {
-			int start = i+1, end = sum-i;
+		for (int i = 1; i <= sum/2; ++i) { // 3 => [1, 2], i <= sum/2
 			vector<int> tmp; tmp.push_back(i);
 			int cnt = i;
 
-			while (start <= end) {
-				for (int j = start; j <= end; ++j) {
-					cnt += j; tmp.push_back(j);
-					if (cnt == sum) { res.push_back(tmp); }
-					if (cnt > sum) { break; }
-				}
-				++start;
-				cnt = i;
-				tmp.clear(); tmp.push_back(i);
+			for (int j = i+1; j < sum; ++j) {
+				cnt += j; tmp.push_back(j);
+				if (cnt == sum) { res.push_back(tmp); }
+				if (cnt > sum) { break; }
 			}
 		}
 		return res;
     }
+
+	int getSum(int start, int end) {
+		int res = 0;
+		for (int i = start; i <= end; ++i) {
+			res += i;
+		}
+		return res;
+	}
+
+	vector<vector<int>> findContinuousSequence(int sum) {
+		if (sum <= 2) { return {}; }
+
+		vector<vector<int>> res;
+		int left = 1, right = 2;
+		while (left <= sum/2 && right < sum) {
+			int cnt = getSum(left, right);
+			if (cnt < sum) {
+				++right;
+			} else {
+				if (cnt == sum) {
+					vector<int> tmp;
+					for (int i = left; i <= right; ++i) { tmp.push_back(i); }
+					res.push_back(tmp);
+				}
+				++left; right = left+1;
+			}
+		}
+
+		return res;
+	}
 };
 
 void show_arr_one_dimensional(vector<int> arr) {
