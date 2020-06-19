e06f873b67b3e9c994dcf6353da9f00dfae6f384
longjianjiang <616393956@qq.com>
2020-06-19 21:52:30 +0800

    dp;

---
+++ interview/interview_08_11.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+// 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+
+class Solution {
+public:
+    int waysToChange(int n) {
+		vector<int> coins = {1, 5, 10, 25};
+
+		vector<int> dp(n+1, 0); 
+		dp[0] = 1;
+		for (int coin: coins) {
+			for (int i = coin; i <= n; ++i) {
+				dp[i] = (dp[i]+dp[i-coin]) % 1000000007;
+			}
+		}
+
+		return dp[n];
+    }
+};
+
+
+void unit_test() {
+	Solution s;
+
+	cout << s.waysToChange(5) << endl;
+	cout << s.waysToChange(10) << endl;
+}
+
+int main() {
+	unit_test();
+	return 0;
+}
