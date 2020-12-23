efdfe604b5c10731041812e6658ca078368ccb75
longjianjiang <616393956@qq.com>
2020-12-23 20:56:48 +0800

    updated 461; bit;

--- cpp/461_hamming_distance.cpp
+++ cpp/461_hamming_distance.cpp
@@ -9,9 +9,30 @@
 #include <unordered_map>
 using namespace std;
 
+// 给定x, y, 要求返回x异或y的值中有多少个1.
+// mask 按位遍历即可。
 class Solution {
 public:
     int hammingDistance(int x, int y) {
+        int z = x ^ y;
+		int cnt = 0;
+		long mask = 1;
+		while (mask <= z) {
+			if (mask & z) { ++cnt; }
+			mask <<= 1;
+		}
 
+		return cnt;
     }
 };
+
+void unit_test() {
+	Solution s;
+
+	cout << s.hammingDistance(1, 4) << endl;
+}
+
+int main() {
+	unit_test();
+	return 0;
+}
