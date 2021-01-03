80220f6570988ad47ea5cca87cdf60ca76d948b7
longjianjiang <616393956@qq.com>
2021-01-03 16:25:09 +0800

    122; dp; sw;

--- cpp/122_best_time_to_buy_and_sell_stock_two.cpp
+++ cpp/122_best_time_to_buy_and_sell_stock_two.cpp
@@ -10,14 +10,11 @@
 using namespace std;
 
 // Input: [7,1,5,3,6,4]
-// Output: 5
-// 给定数组表示每一天的股票的价格，只允许交易一次，返回收益最大的交易。
-// 1> dp的方式； dp[i] 表示第i天的收益最大。 dp[i][0] 表示卖出，dp[i][1] 表示买进。
-// 2> 滑动窗口；
+// Output: 7
+// 和121类似，区别在于可以进行多笔交易；
 class Solution {
 public:
-
-    int maxProfit_dp(vector<int>& prices) {
+	int maxProfit_dp(vector<int>& prices) {
 		int nsize = prices.size();
 		if (nsize < 2) { return 0; }
 
@@ -26,13 +23,11 @@ public:
 		dp[0][0] = 0; dp[0][1] = -prices[0];
 
 		for (int i = 1; i < nsize; ++i) {
-			dp[i][0] = max(dp[i-1][0], prices[i]+dp[i-1][1]);
-			dp[i][1] = max(dp[i-1][1], -prices[i]);
-			res = max(res, dp[i][0]);
+			dp[i][0] = max(dp[i-1][0], prices[i] + dp[i-1][1]);
+			dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
 		}
-
 		return res;
-    }
+	}
 
 	int maxProfit_sw(vector<int>& prices) {
 		int nsize = prices.size();
@@ -40,14 +35,20 @@ public:
 
 		int res = 0;
 		int l = 0, r = 1;
+
 		while (r < nsize) {
 			if (prices[r] > prices[l]) {
-				res = max(res, prices[r] - prices[l]);
+				if (r+1 < nsize && prices[r+1] < prices[r]) {
+					res += prices[r] - prices[l];
+					l = r;
+				}
 			} else {
 				l = r;
 			}
 			++r;
 		}
+		// case: [1, 2, 3, 4]
+		res += prices[r-1] - prices[l];
 
 		return res;
 	}
