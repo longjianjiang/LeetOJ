20cdf433af7c4f1ea23de81a7618ca0cb8ad09ca
longjianjiang <616393956@qq.com>
2021-02-28 00:06:35 +0800

    496; stack; rv;

---
+++ review/rv_496.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+/*
+ 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ 输出: [-1,3,-1]
+ */
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> stk;
+        nums2.push_back(INT_MAX);
+		int nsize2 = nums2.size();
+		unordered_map<int, int> dict;
+
+		// 4 1 2 3
+		for (int i = 0; i < nsize2; ++i) {
+            int num = nums2[i] == INT_MAX ? -1 : nums2[i];
+            while (!stk.empty() && nums2[i] > stk.top()) {
+                dict[stk.top()] = num;
+                stk.pop();
+            }
+            stk.push(num);
+		}
+
+		vector<int> res;
+		for (int num: nums1) {
+            res.push_back(dict[num]);
+			//res.push_back(dict.count(num) ? dict[num] : -1);
+		}
+
+		return res;    }
+};
