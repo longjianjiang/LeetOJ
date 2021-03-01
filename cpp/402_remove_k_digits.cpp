8da57a9103ff5bf2b8da655a55c1bb4b14ca490c
longjianjiang <616393956@qq.com>
2021-03-02 00:36:53 +0800

    Update

---
+++ cpp/402_remove_k_digits.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+// 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+/*
+输入: num = "1432219", k = 3
+输出: "1219"
+解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+*/
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+    }
+};
