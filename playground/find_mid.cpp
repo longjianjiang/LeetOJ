522cd870cfe49a1ed2f42f413ccfa1bef2b0928d
longjianjiang <616393956@qq.com>
2021-03-17 23:49:32 +0800

    Update

---
+++ playground/find_mid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+int find_mid(vector<int>& nums) {
+	return -1;
+}
+
+int main() {
+	vector<int> list1 = {3, 1, 2, 5, 4};
+	int mid1 = find_mid(list1);
+
+	assert(mid1 == 3);
+	return 0;
+}
