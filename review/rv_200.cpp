345a890593833cf745c4cc00ef187d08e55ca203
longjianjiang <616393956@qq.com>
2021-03-22 21:13:53 +0800

    rv; 200; bfs;

--- review/rv_200.cpp
+++ review/rv_200.cpp
@@ -11,10 +11,70 @@ using namespace std;
 
 // 给定矩阵，求岛屿数量，所谓岛屿就是被0包围着的1的连通图。
 // 1> 使用union find，先进行合并，然后查找1所构成的union有多少个。
+// 2> 使用dfs，一次搜索递归的把周围的1改成其他字符，统计个数。
 class Solution {
 public:
 	vector<pair<int, int>> edges = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
+	// bfs
+	void bfs(vector<vector<char>>& grid, int x, int y) {
+		int row = grid.size();
+		if (row == 0) { return; }
+		int col = grid[0].size();
+		if (col == 0) { return; }
+
+		if (x < 0 || x >= row || y < 0 || y >= col) { return; }
+		if (grid[x][y] != '1') { return; }
+
+		grid[x][y] = '$';
+		queue<pair<int, int>> q;
+		q.push({x, y});
+
+		while (!q.empty()) {
+			int nsize = q.size();
+			for (int i = 0; i < nsize; ++i) {
+				auto point = q.front(); q.pop();
+				int x2 = point.first, y2 = point.second;
+				if (y2+1 < col && grid[x2][y2+1] == '1') {
+					q.push({x2, y2+1});
+					grid[x2][y2+1] = '$';
+				}
+				if (x2+1 < row && grid[x2+1][y2] == '1') {
+					q.push({x2+1, y2});
+					grid[x2+1][y2] = '$';
+				}
+				if (x2-1 >= 0 && grid[x2-1][y2] == '1') {
+					q.push({x2-1, y2});
+					grid[x2-1][y2] = '$';
+				}
+				if (y2-1 >= 0 && grid[x2][y2-1] == '1') {
+					q.push({x2, y2-1});
+					grid[x2][y2-1] = '$';
+				}
+			}
+		}
+	}
+
+	int numIslands(vector<vector<char>>& grid) {
+		int row = grid.size();
+		if (row == 0) { return 0; }
+		int col = grid[0].size();
+		if (col == 0) { return 0; }
+
+		int res = 0;
+		for (int i = 0; i < row; ++i) {
+			for (int j = 0; j < col; ++j) {
+				if (grid[i][j] == '1') {
+					bfs(grid, i, j);
+					res += 1;
+				}
+			}
+		}
+
+		return res;
+	}
+
+	// dfs
 	void dfs(vector<vector<char>>& grid, int x, int y) {
 		int row = grid.size();
 		if (row == 0) { return; }
