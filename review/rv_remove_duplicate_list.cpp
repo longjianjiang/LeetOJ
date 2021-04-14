cd2ce2ee4f06fcb3866bdfc9aa350994022dfdef
longjianjiang <616393956@qq.com>
2021-04-15 00:22:32 +0800

    Update

--- review/rv_remove_duplicate_list.cpp
+++ review/rv_remove_duplicate_list.cpp
@@ -58,3 +58,32 @@ ListNode *removeDuplicates(ListNode *head) {
 
 	return head;
 }
+
+// 0-1-2-2-3-4-4-5
+// 0-1-1-2-2-3
+// 0-1-1-1
+ListNode *deleteDuplicates(ListNode *head) {
+	if (!head || !head->next) { return head; }
+
+	auto tmp = new ListNode(head->val-1);
+	tmp->next = head;
+
+	auto p = tmp, q = head;
+	while (q) {
+		while (q->next && q->next->val == q->val) {
+			q = q->next;
+		}
+		// p，q不相等，正常移动；
+		if (p->next == q) {
+			p = p->next;
+		} 
+		// 此时q指向的是最后的重复元素，p是重复元素序列第一个的前一个，所以直接进行整个删除；
+		else {
+			p->next = q->next;
+		}
+		// 正常移动q，执行下一个
+		q = q->next;
+	}
+
+	return tmp->next;
+}
