2de9c70bc4ea3f9fb837788021694f5a5eaa6ca7
longjianjiang <616393956@qq.com>
2021-06-15 23:58:13 +0800

    Update

---
+++ interview/interview_16_07.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+// 编写一个方法，找出两个数字a和b中最大的那一个。不得使用if-else或其他比较运算符。
+
+class Solution {
+public:
+    int maximum(int a, int b) {
+    	long diff = (long)a - (long)b;
+		auto index = to_string(diff)[0];
+
+		unordered_map<char, int> dict;
+		dict['-'] = b;
+		for (int i = 0; i <= 9; ++i) { dict[i + '0'] = a; }
+
+		return dict[index];
+    }
+};
+
+void unit_test() {
+	Solution s;
+
+	cout << s.maximum(2, 1) << endl;
+}
+
+int main() {
+	unit_test();
+	return 0;
+}
