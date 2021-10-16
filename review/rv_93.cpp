e01c2e03954f379672c109d67993012a8f3c3139
longjianjiang <616393956@qq.com>
2021-10-17 00:43:59 +0800

    Update

--- review/rv_93.cpp
+++ review/rv_93.cpp
@@ -16,18 +16,23 @@ public:
 		if (idx > nsize || ip.size() > 4) { return; }
 		if (nsize == idx && ip.size() == 4) {
 			auto tmp = ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3];
-			res.push_back(res);
+			res.push_back(tmp);
+			return;
 		}
 
-		if (int len = 1; len <= 3; ++len) {
+		for (int len = 1; len <= 3; ++len) {
 			string sub = s.substr(idx, len);
+            if (sub.empty() || (sub.size() > 1 && sub[0] == '0') || stoi(sub) > 255) { continue; }
+			ip.push_back(sub);
+			dfs(s, res, ip, idx+len);
+			ip.pop_back();
 		}
 	}
 
     vector<string> restoreIpAddresses(string s) {
 		if (s.size() > 12) { return {}; }
 
-		vector<int> res;
+		vector<string> res;
 		dfs(s, res, {}, 0);
 
 		return res;
