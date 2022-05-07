24d2434c62ace6e8baaa1478ae7f5ffc65706dce
longjianjiang <616393956@qq.com>
2022-05-08 00:50:35 +0800

    maximum subarray dc;

--- review/rv_53_maximum_subarray.cpp
+++ review/rv_53_maximum_subarray.cpp
@@ -26,4 +26,37 @@ public:
 
 		return maxSum;
     }
+
+	int helper(vector<int>& nums, int left, int right) {
+		if (left == right) { return nums[left]; }
+		
+		int mid = left + (right - left) / 2;
+		int leftSum = helper(nums, left, mid);
+		int rightSum = helper(nums, mid+1, right);
+
+		int leftSuffixMax = nums[mid];
+		int leftSuffixSum = leftSuffixMax;
+		for (int i = mid-1; i >= left; --i) {
+			leftSuffixSum += nums[i];
+			leftSuffixMax = max(leftSuffixSum, leftSuffixMax);
+		}
+
+		int rightPrefixMax = nums[mid+1];
+		int rightPrefixSum = rightPrefixMax;
+		for (int i = mid+2; i <= right; ++i) {
+			rightPrefixSum += nums[i];
+			rightPrefixMax = max(rightPrefixSum, rightPrefixMax);
+		}
+
+		int rangeSum = leftSuffixMax + rightPrefixMax;
+		return max(rangeSum, max(leftSum, rightSum));
+	}
+
+	int maxSubArray(vector<int>& nums) {
+		int nsize = nums.size();
+		if (nsize == 0) { return 0; }
+		if (nsize == 1) { return nums[0]; }
+
+		return helper(nums, 0, nsize-1);
+	}
 };
