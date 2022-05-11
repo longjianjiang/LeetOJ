58b8a79602c94757b419f198c9b7b548f3972759
longjianjiang <616393956@qq.com>
2022-05-11 14:30:40 +0800

    sliding window;

--- cpp/438_find_all_anagrams_in_a_string.cpp
+++ cpp/438_find_all_anagrams_in_a_string.cpp
@@ -30,4 +30,42 @@ public:
 
 		return res;
     }
+
+	vector<int> findAnagrams(string s, string p) {
+		vector<int> res;
+		if (p.size() > s.size()) { return res; }
+
+		unordered_map<char, int> um;
+		for (auto ch : p) {
+			um[ch]++;
+		}
+		int cnt = um.size();
+		int l = 0, r = 0;
+
+		while (r < s.size()) {
+			auto ch = s[r];
+			if (um.find(ch) != um.end()) {
+				um[ch]--;
+				if (um[ch] == 0) {
+					cnt--;
+				}
+			}
+			while (cnt == 0) {
+				if (r - l + 1 == p.size()) {
+					res.push_back(l);
+				}
+				auto ch = s[l];
+				if (um.find(ch) != um.end()) {
+					um[ch]++;
+					if (um[ch] == 1) {
+						cnt++;
+					}
+				}
+				l++;
+			}
+			r++;
+		}
+
+		return res;
+	}
 };
