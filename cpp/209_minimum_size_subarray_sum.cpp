f9a1e458a5bd95b4c3f05c5ce33eeb71f346092f
longjianjiang <616393956@qq.com>
2022-05-15 00:09:20 +0800

    209; binary search;

--- cpp/209_minimum_size_subarray_sum.cpp
+++ cpp/209_minimum_size_subarray_sum.cpp
@@ -27,4 +27,39 @@ public:
 
 		return cnt == INT_MAX ? 0 : cnt;
 	}
+
+	int bs(vector<int>& prefix, int sum, int left, int right) {
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (prefix[mid] >= sum) {
+				if (mid == 0 || prefix[mid-1] < sum) {
+					return mid;
+				} else {
+					right = mid - 1;
+				}
+			} else {
+				left = mid + 1;
+			}
+		}
+		return -1;
+	}
+
+	int minSubArrayLenBS(int s, vector<int>& nums) {
+		int nsize = nums.size();
+		
+		vector<int> prefix(nsize+1, 0);
+		for (int i = 0; i < nsize; ++i) {
+			if (nums[i] >= s) { return 1; }
+			prefix[i+1] = prefix[i] + nums[i];
+		}
+
+		int minLen = INT_MAX;
+		for (int i = 0; i < nsize; ++i) {
+			int pos = bs(prefix, prefix[i]+s, i+1, nsize);
+			if (pos == -1) { continue; }
+			minLen = min(minLen, pos - i);
+		}
+		
+		return minLen == INT_MAX ? 0 : minLen;
+	}
 };
