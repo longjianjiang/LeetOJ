3e8ef4c15304704a4ab1149d43719047f57f6712
longjianjiang <616393956@qq.com>
2022-05-16 23:07:04 +0800

    93; dfs; string;

--- review/rv_93.cpp
+++ review/rv_93.cpp
@@ -10,22 +10,30 @@
 using namespace std;
 
 class Solution {
+private:
+		int ipSize = 4;
 public:
-	void dfs(string s, vector<string>& res, vector<string> ip, int idx) {
+	// "101023"
+	void dfs(string s, vector<string>& res, vector<string> chosen, int idx) {
 		int nsize = s.size();
-		if (idx > nsize || ip.size() > 4) { return; }
-		if (nsize == idx && ip.size() == 4) {
-			auto tmp = ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3];
-			res.push_back(tmp);
+		if (idx > nsize) { return; }
+		if (chosen.size() > ipSize) { return; }
+		if (idx == nsize && chosen.size() == ipSize) {
+			string ip = "";
+			for (int i = 0; i < ipSize; ++i) {
+				ip += chosen[i];
+				if (i < ipSize-1) { ip += "."; }
+			}
+			res.push_back(ip);
 			return;
 		}
-
 		for (int len = 1; len <= 3; ++len) {
 			string sub = s.substr(idx, len);
-            if (sub.empty() || (sub.size() > 1 && sub[0] == '0') || stoi(sub) > 255) { continue; }
-			ip.push_back(sub);
-			dfs(s, res, ip, idx+len);
-			ip.pop_back();
+			if (sub.empty() || (sub.size() > 1 && sub[0] == '0') || stoi(sub) > 255) { continue; }
+
+			chosen.push_back(sub);
+			dfs(s, res, chosen, idx+len);
+			chosen.pop_back();
 		}
 	}
 
@@ -38,3 +46,36 @@ public:
 		return res;
     }
 };
+
+template<typename T>
+void show_arr_one_dimensional(vector<T> arr) {
+	cout << "[";
+	for (int i = 0; i < arr.size(); ++i) {
+		T num = arr[i];
+		if (i == arr.size()-1) {
+			cout << num;
+		} else {
+			cout << num << " ";
+		}
+	}
+	cout << "]\n";
+}
+
+template<typename T>
+void show_arr_two_dimensional(vector<vector<T>> arr) {
+	for (auto v: arr) {
+		show_arr_one_dimensional(v);
+	}
+}
+
+void unit_test() {
+	Solution s;
+	auto res = s.restoreIpAddresses("101023");
+	
+	show_arr_one_dimensional(res);
+}
+
+int main() {
+	unit_test();
+	return 0;
+}
