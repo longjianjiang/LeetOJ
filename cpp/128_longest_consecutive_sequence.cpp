bfd18f256121499f730284d36d6f1f74bea72e8a
longjianjiang <616393956@qq.com>
2022-05-21 21:39:28 +0800

    128; union find;

--- cpp/128_longest_consecutive_sequence.cpp
+++ cpp/128_longest_consecutive_sequence.cpp
@@ -21,6 +21,48 @@ using namespace std;
 
 class Solution {
 public:
+	// union find.
+	// union : consecutive sequence;
+	// find : num, num+1, union(num, num+1); um[num+1] = num;
+	int longestConsecutiveUnionFind(vector<int>& nums) {
+		unordered_map<int, int> um;
+		unordered_map<int, int> cnt;
+		for (auto num : nums) {
+			um[num] = num;
+			cnt[num] = 1;
+		}
+
+		auto findFunc = [&](int i) {
+			while (i != um[i]) {
+				um[i] = um[um[i]];
+				i = um[i];
+			}
+			return i;
+		};
+		
+		// 100 4 200 1 3 2
+		// 1   1  1  1 2 1
+		auto unionFunc = [&](int i, int j) {
+			int p = findFunc(i), q = findFunc(j);
+			if (p != q) {
+				um[q] = p;
+				cnt[p] += cnt[q];
+			}
+		};
+		
+		for (auto num : nums) {
+			if (um.find(num+1) != um.end()) {
+				unionFunc(num, num+1);
+			}
+		}
+
+		int res = 0;
+		for (auto x : cnt) {
+			res = max(res, x.second);
+		}
+		return res;
+	}
+
     int longestConsecutive(vector<int>& nums) {
 		unordered_set<int> us;
 		for (auto num : nums) {
@@ -43,3 +85,15 @@ public:
 		return res;
     }
 };
+
+void unit_test() {
+	Solution s;
+
+	vector<int> nums = {100, 4, 200, 2, 1, 3};
+	cout << s.longestConsecutiveUnionFind(nums) << endl;
+}
+
+int main() {
+	unit_test();
+	return 0;
+}
