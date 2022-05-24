47939e917480772d35b8d2c6b8976c226d7862ba
longjianjiang <616393956@qq.com>
2022-05-24 23:43:53 +0800

    236; tree;

---
+++ review/rv_236.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (!root) { return NULL; }
+		if (root == p || root == q) { return root; }
+
+		auto left = lowestCommonAncestor(root->left, p, q);
+		auto right = lowestCommonAncestor(root->right, p, q);
+
+		if (left && right) {
+			return root;
+		} else if (!left) {
+			return right;
+		} else if (!right) {
+			return left;
+		} else {
+			return NULL;
+		}
+    }
+};
