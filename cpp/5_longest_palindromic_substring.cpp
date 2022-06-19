892bf30de06ab91c76343b5bf036259cdb7626a7
longjianjiang <616393956@qq.com>
2022-06-19 16:47:31 +0800

    5; update;

--- cpp/5_longest_palindromic_substring.cpp
+++ cpp/5_longest_palindromic_substring.cpp
@@ -12,9 +12,33 @@ using namespace std;
 // 给你一个字符串 s，找到 s 中最长的回文子串。
 // s = "babad"
 // 1> 遍历，根据回文的特点进行两端进行移动，定位到子串;
+// 2> 还是1的思路，去除了使用set的逻辑，每次遍历确定好左右边界，然后两端移动，定位子串即可；
 class Solution {
 public:
-    string longestPalindrome(string s) {
+	string longestPalindrome(string s) {
+		int nsize = s.size();
+        if (nsize <= 1) { return s; }
+
+		auto findSubstr = [&](string s, int l, int r) {
+			while (l - 1 >= 0 && r + 1 < nsize && s[l-1] == s[r+1]) {
+				--l; ++r;
+			}
+			return s.substr(l, r-l+1);
+		};
+        string res = s.substr(0, 1);
+		for (int i = 0; i < nsize; ++i) {
+			int r = i;
+			while (r + 1 < nsize && s[r+1] == s[r]) {
+				++r;
+			}
+			string tmp = findSubstr(s, i, r);
+			if (tmp.size() > res.size()) {
+				res = tmp;
+			}
+		}
+		return res;
+	}
+    string longestPalindrome2(string s) {
 		int nsize = s.size();
         if (nsize <= 1) { return s; }
 
@@ -69,7 +93,7 @@ void unit_test() {
 	cout << s.longestPalindrome("aabba") << endl;
 	cout << s.longestPalindrome("abcba") << endl;
 	cout << s.longestPalindrome("abba") << endl;
-	cout << s.longestPalindrome("babad") << endl;
+	cout << s.longestPalindrome("sooos") << endl;
 }
 
 int main() {
