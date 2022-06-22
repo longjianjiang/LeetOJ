f47c4e3ee66312530694310824f219218ca2bdf9
longjianjiang <616393956@qq.com>
2022-06-23 01:41:08 +0800

    647; dp;

--- cpp/647_palindromic_substrings.cpp
+++ cpp/647_palindromic_substrings.cpp
@@ -13,11 +13,43 @@ using namespace std;
 // 1> brute force；
 class Solution {
 public:
+	int countSubstrings(string s) {
+		int nsize = s.size();
+		if (nsize <= 1) { return nsize; }
 
-    int countSubstrings(string s) {
+		int res = nsize;
+		vector<vector<int>> dp(nsize+1, vector<int>(nsize+1, 0));
+		// dp[idx][len]
+		for (int i = 0; i < nsize; ++i) {
+			dp[i][0] = 1;
+			dp[i][1] = 1;
+		}
+		// "abc"
+		// len = 2, idx = 0; dp[1][0] s[1] s[0]
+		// len = 2, idx = 1; dp[2][0] s[1] s[1]
+		for (int len = 2; len <= nsize; ++len) {
+			for (int idx = 0; idx < nsize; ++idx) {
+				if (idx + len <= nsize) {
+					dp[idx][len] = dp[idx+1][len-2] && s[idx+len-1] == s[idx];
+					if (dp[idx][len]) {
+						res++;
+					}
+				}
+			}
+		}
+		return res;
+	}
+    int countSubstrings2(string s) {
 		// "aaa"
-		// "aabb"
 		// "aba"
+		// "aaaaa"
+		// "aaaba"
+		// "sooos"
+
+		// "aa"
+		// "aaba"
+		// "baab"
+		// "abab"
 
 		int nsize = s.size();
 		if (nsize <= 1) { return nsize; }
