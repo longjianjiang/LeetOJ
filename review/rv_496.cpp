9c4f7178efaa0f5bbebfd9b58ae91a6535743b7a
longjianjiang <616393956@qq.com>
2022-07-17 19:04:18 +0800

    496; monotone stack;

--- review/rv_496.cpp
+++ review/rv_496.cpp
@@ -17,26 +17,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> stk;
-        nums2.push_back(INT_MAX);
-		int nsize2 = nums2.size();
+		//nums2.push_back(INT_MAX);
+        int nsize2 = nums2.size();
+		stack<int> stk;
 		unordered_map<int, int> dict;
-
-		// 4 1 2 3
 		for (int i = 0; i < nsize2; ++i) {
-            int num = nums2[i] == INT_MAX ? -1 : nums2[i];
-            while (!stk.empty() && nums2[i] > stk.top()) {
-                dict[stk.top()] = num;
-                stk.pop();
-            }
-            stk.push(num);
+			int num = nums2[i];
+			while (!stk.empty() && num > stk.top()) {
+				dict[stk.top()] = num;
+				stk.pop();
+			}
+			stk.push(num);
 		}
 
 		vector<int> res;
-		for (int num: nums1) {
-            res.push_back(dict[num]);
-			//res.push_back(dict.count(num) ? dict[num] : -1);
+		for (auto num : nums1) {
+			if (dict.find(num) != dict.end()) {
+				res.push_back(dict[num]);
+			} else {
+				res.push_back(-1);
+			}
 		}
-
-		return res;    }
+		return res;
+    }
 };
