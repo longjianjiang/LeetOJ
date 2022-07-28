7e88cfe6a14f7409e1ef7ece54c2b5fee172b4d1
longjianjiang <616393956@qq.com>
2022-07-28 22:33:56 +0800

    Update;

--- review/rv_119.cpp
+++ review/rv_119.cpp
@@ -20,18 +20,19 @@ public:
 
 		int cnt = rowIndex - 1;
 		while (cnt) {
-			vector<int> tmp = arr.back();
+            vector<int> tmp = arr.back();
+
 			vector<int> added;
 			added.push_back(1);
 			for (int i = 0; i < tmp.size()-1; ++i) {
 				int sum = tmp[i] + tmp[i+1];
 				added.push_back(sum);
 			}
-
 			added.push_back(1);
+
 			arr.push_back(added);
 
-			++cnt;
+			--cnt;
 		}
 		return arr[rowIndex];
     }
