8a7ca94df8eca0897880bf03e457cce0f4ae5a50
longjianjiang <616393956@qq.com>
2022-09-14 00:06:25 +0800

    Update;

--- cpp/1385-find-the-distance-value-between-two-arrays.cpp
+++ cpp/1385-find-the-distance-value-between-two-arrays.cpp
@@ -11,8 +11,35 @@ using namespace std;
 
 class Solution {
 public:
+    int findPeakElement(vector<int>& nums) {        
+        int n=(int)nums.size();
+        if(n==1 || nums[0]>nums[1]) return 0;
+        else if(nums[n-1]>nums[n-2]) return n-1;
+        int lo=1, hi=n-2, mid;
+        while(lo<=hi){
+            mid=(lo+hi)/2;
+            if(nums[mid]>max(nums[mid-1],nums[mid+1])) return mid;
+            else if(nums[mid]<nums[mid-1]) hi=mid-1;
+            else lo=mid+1;
+        }
+        return -1;
+
+        // int left = 0, right = nums.size()-1;
+
+        // while (left < right) {
+        //     int mid = left + (right-left)/2;
+        //     if (nums[mid] < nums[mid+1]) {
+        //         left = mid+1;
+        //     } else {
+        //         right = mid;
+        //     }
+        // }
+
+        // return left; // right 
+    }
+
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-		        sort(arr2.begin(), arr2.end());
+		sort(arr2.begin(), arr2.end());
 
         int nsize1 = arr1.size(), nsize2 = arr2.size();
         int res = 0;
@@ -44,3 +71,16 @@ public:
         return res;
     }
 };
+
+void unit_test() {
+	Solution s;
+	vector<int> arr1 = {1, 2, 3, 3, 4, 3};
+	vector<int> arr2 = {1, 2, 3, 4, 3, 3};
+	cout << s.findPeakElement(arr1) << endl;
+	cout << s.findPeakElement(arr2) << endl;
+}
+
+int main() {
+	unit_test();
+	return 0;
+}
