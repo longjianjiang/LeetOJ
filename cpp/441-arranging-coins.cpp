01f76a5bd6dc2ca9b3d4ae21c76922313d7fbb1b
longjianjiang <616393956@qq.com>
2022-10-11 23:29:27 +0800

    Update

--- cpp/441-arranging-coins.cpp
+++ cpp/441-arranging-coins.cpp
@@ -11,11 +11,11 @@ using namespace std;
 
 class Solution {
 public:
-    int getSum(int n) {
+    long long getSum(int n) {
         if (n <= 1) { return n; }
 
         int i = 1; int j = n;
-        int sum = 0;
+        long long sum = 0;
         while (i < j) {
             sum += (i + j);
             ++i; --j;
@@ -36,11 +36,13 @@ public:
         int l = 1, r = n / 2 + 1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            int sum = getSum(l);
-            if (sum >= n) {
+            int sum = getSum(mid);
+            if (sum == n) {
                 return mid;
+            } else if (sum > n) {
+                return mid - 1;
             } else {
-                l++;
+                l = mid + 1;
             }
         }
         return -1;
