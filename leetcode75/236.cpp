e291baee31e1f2ed1e0aa55390f3d78cef59ea9e
longjianjiang <616393956@qq.com>
2023-12-17 17:07:00 +0800

    review 236, tree, recursive;

---
+++ leetcode75/236.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+/*
+ * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (!root) { return NULL; }
+		if (root == p || root == q) { return root; }
+		TreeNode *l = lowestCommonAncestor(root->left, p, q);
+		TreeNode *r = lowestCommonAncestor(root->right, p, q);
+		if (l && r) {
+			return root;
+		}
+		if (l) {
+			return l;
+		}
+		if (r) {
+			return r;
+		}
+		return NULL;;
+    }
+};
